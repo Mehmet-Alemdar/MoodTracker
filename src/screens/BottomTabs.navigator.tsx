1f35202bccb8299d0311515d515a9ec025cd0319
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-09-30 17:32:39 +0300

    Add bottom nav icons

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -3,15 +3,36 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from './Home.screen';
 import { History } from './History.screen';
 import { Analytics } from './Analytics.screen';
+import { IconHome, IconHistory, IconAnalytics } from '../components/Icons';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator()
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="History" component={History} />
-      <BottomTabs.Screen name="Analytics" component={Analytics} />
+    <BottomTabs.Navigator
+      screenOptions={({route}) => ({
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({color, size}) => {
+          if(route.name === 'Home') {
+            return <IconHome color={color} size={size}/>
+          }
+          if(route.name === 'History') {
+            return <IconHistory color={color} size={size}/>
+          }
+          if(route.name === 'Analytics') {
+            return <IconAnalytics color={color} size={size}/>
+          }
+
+          return null
+        }
+      })}
+    >
+      <BottomTabs.Screen name="Home" component={Home} options={{title: 'Today\'s Mood'}}/>
+      <BottomTabs.Screen name="History" component={History} options={{title: 'Past Moods'}}/>
+      <BottomTabs.Screen name="Analytics" component={Analytics} options={{title: 'Fancy Charts'}} />
     </BottomTabs.Navigator>
   )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,7 @@
 export const theme = {
   colorPurple: ' #454c73',
   colorWhite: '#fff',
-  colorLavender: '#87677B'
+  colorLavender: '#87677B',
+  colorBlue: '#1D84B5',
+  colorGrey: '#8E9AAF',
 }
